4a3c08ccf54b4edd6898e6cad8922b9f87458dd4
Sahar El-behiry <sahar.mis2015@gmail.com>
2022-07-25 14:34:34 +0200

    created basic layout

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,17 @@
 </script>
 
 <template>
-
+    <main class="main_container">dfd</main>
 </template>
 
 <style scoped lang="scss">
 
+@import './assets/variables';
+    .main_container {
+        width: 95%;
+        max-width: 456px;
+        min-height: 70vh;
+        border-radius: $container_border_radius;
+
+    }
 </style>
