aaf203e363c9b6f76f24bd5cafb2192b4a402322
Sahar El-behiry <sahar.mis2015@gmail.com>
2022-07-26 10:51:10 +0200

    extracted transition classes into scss file

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -65,34 +65,4 @@ function deletetask(task) {
         margin-bottom: 20rem;
     }
 }
-
-//transition styles
-.list-enter-active,
-.list-leave-active {
-    transition: all 0.5s ease;
-}
-
-.list-enter-from,
-.list-leave-to {
-    opacity: 0;
-    transform: translateX(30px);
-}
-.list-move,
-/* apply transition to moving elements */
-.list-enter-active,
-.list-leave-active {
-    transition: all 0.5s ease;
-}
-
-.list-enter-from,
-.list-leave-to {
-    opacity: 0;
-    transform: translateX(30px);
-}
-
-/* ensure leaving items are taken out of layout flow so that moving
-   animations can be calculated correctly. */
-.list-leave-active {
-    position: absolute;
-}
 </style>
