ebbd55873623a8c7a27eedf83db2a8f81d261234
Sahar El-behiry <sahar.mis2015@gmail.com>
2022-07-26 19:48:06 +0200

    deleted static datat and get datat from local host

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,24 +1,15 @@
 <script setup>
-import { reactive } from 'vue'
+import { reactive, watchEffect } from 'vue'
 import TodoItem from './TodoItem.vue';
+import AddTaskForm from './AddTaskForm.vue';
 
-const todoList = reactive([
-    {
-        id: 1,
-        title: 'task titlehguihioh nojhio j;oi oiujoikj oiuoiu iouiou ',
-        done: false
-    },
-    {
-        id: 2,
-        title: 'task title',
-        done: false
-    },
-    {
-        id: 3,
-        title: 'task title',
-        done: false
-    },
-])
+const local_data_key = 'localTodoList';
+const todoList = reactive([]);
+
+//get data from local storage if exists
+todoList.value = JSON.parse(localStorage.getItem(local_data_key)) ?? []
+
+
 
 /*
 ** 1- move uncompleted task to the top
@@ -28,9 +19,9 @@ function toggleTaskState(task) {
     deletetask(task)
 
     if(task.done) {
-        todoList.push(task)
+        todoList.value.push(task)
     } else {
-        todoList.unshift(task)
+        todoList.unshift.value(task)
     }
 }
 
@@ -39,20 +30,41 @@ function toggleTaskState(task) {
 ** affected the original array
 */
 function deletetask(task) {
-    let taskIndex = todoList.indexOf(task);
-    todoList.splice(taskIndex, 1);
+    let taskIndex = todoList.value.indexOf(task);
+    todoList.value.splice(taskIndex, 1);
 }
+
+//add new task
+function addNewTask(task) {
+    let taskObject = {
+        id: todoList.value.length + 1,
+        title: task,
+        done: false
+    }
+
+    todoList.value.unshift(taskObject);
+}
+
+
+//update local storage item on todoList changed
+watchEffect(() => {
+    localStorage.setItem(local_data_key, JSON.stringify(todoList.value))
+})
 
 </script>
 
 <template>
     <section class="todo_list_conatiner">
         <h3 class="title">0/3 done</h3>
+        <!-- todo items -->
         <transition-group tag="ul" name="list">
-            <TodoItem v-for="item in todoList" :taskDetails="item" :key="item.id" @stateChanged="toggleTaskState(item)"
-                @deleteTask="deletetask(item)">
+            <TodoItem v-for="item in todoList.value" :taskDetails="item" :key="item.id"
+                @stateChanged="toggleTaskState(item)" @deleteTask="deletetask(item)">
             </TodoItem>
         </transition-group>
+
+        <!-- add todo item form -->
+        <AddTaskForm @newTaskAdded="addNewTask"></AddTaskForm>
 
     </section>
 </template>
